cf1deaeac2e156d8a36c4e1caf00faf7d4aced26
Gergely20 <ngergely065@gmail.com>
2021-10-08 18:22:18 +0200

    Az alap szerver elkészült

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const http = require ('http');
 const fs = require ('fs');
 const port = 4444
+
+const server = http.createServer( (req, res) => {
+    console.log(req.url);
+    console.log(res.method);
+});
+
+server.listen( port);
